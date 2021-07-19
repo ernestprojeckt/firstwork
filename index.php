cb7835c00849bd9eb07b1acb2ddab01b5562923b
ernest <vurvant337@gmail.com>
2021-07-19 16:23:44 +0300

    init

--- index.php
+++ index.php
@@ -1,5 +1,60 @@
 <?php
-echo 7%3 . "<br>";
 
-echo (int) (7+7.15);
-////
+echo "Get remainder of division (7 divided by 3) = ". 7%3 . "<br>";
+
+echo "Get the whole part of the addition (7 divided by 7.15) = " .(int) (7+7.15). "<br>";
+
+$num = sqrt(25) . "<br>";
+
+
+echo "SQRT from 25 = ". $num. PHP_EOL;
+
+$string = "Десять негритят пошли купаться в море";
+
+$array = explode(" ",$string);
+echo "4 word from string = ". $array[3]. "<br>" ;
+
+$charstring=mb_substr($string,17,1);
+echo "17 char opn string = " . $charstring . "<br>";
+
+$Capitalletters = mb_convert_case($string, MB_CASE_TITLE, 'UTF-8'). PHP_EOL;
+echo "Capital letters = " . $Capitalletters . "<br>";
+
+$linelength = mb_strlen($string);
+echo "Line length = " .  $linelength . "<br>";
+
+//echo assert (true == 1);
+echo "Equation (true equals 1) = ";
+if (true == 1) {
+    echo "true". "<br>";
+} else {
+    echo "false". "<br>";
+}
+//"<br>"
+
+
+echo "Equation (false equals 0) = ";
+if (false === 0) {
+    echo "true". "<br>";;
+} else {
+    echo "false". "<br>";;
+}
+
+echo "Equation (three long три ) = ";
+if (three < три) {
+    echo "true". "<br>";
+} else {
+    echo "false". "<br>";
+}
+
+$example = 125*13+7;
+$example2 = 223+28*2;
+//echo $example ;
+//echo $example2 ;
+echo "Equation (125*13+7)<(223+28*2) = ";
+if ($example2 < $example) {
+    echo "true". "<br>";
+} else {
+    echo "false". "<br>";
+}
+
